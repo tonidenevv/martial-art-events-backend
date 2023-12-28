20e266a35ca5e1e8b4fbc8edbf2f60f729032be1
Toni Denev <tonidenevbtc@gmail.com>
2023-12-28 19:54:46 +0200

    Create Atlas Cluster and connect to it

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const eventsRoutes = require('./routes/eventsRoutes');
 const usersRoutes = require('./routes/usersRoutes')
+
+mongoose.connect(process.env.CONNECTION_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('DB Connected!');
+})
 
 const app = express();
 app.use(cors());

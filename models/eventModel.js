767d28cdb590cb35a7a68660a9e39c4dadb35be2
Toni Denev <tonidenevbtc@gmail.com>
2023-12-29 14:31:42 +0200

    Create user model

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validators = require('../helpers/validators');
 
 const disallowedCharactersValidator = (value) => {
     const disallowedChars = /[!@#$%^&*(),.?":{}|<>]/;

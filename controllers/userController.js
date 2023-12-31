e84dde2dbf733253878375ffb4efe3af391bf5b5
Toni Denev <tonidenevbtc@gmail.com>
2023-12-31 13:16:19 +0200

    Add register functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,28 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const login = (req, res) => {
     res.send('Login...')
 };
 
-const register = (req, res) => {
-    res.send('Register...');
+const register = async (req, res) => {
+    const { username, password, confirmPassword } = req.body;
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+        res.status(409).json({ message: 'Username is already taken' });
+    } else {
+        if (password !== confirmPassword) {
+            res.status(400).json({ message: 'Passwords don\'t match' });
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = await new User(username, hashedPassword);
+            await user.save();
+            const token = jwt.sign({ _id: user._id, username: user.username });
+            res.status(201).json({ token, _id: user._id, username: user.username });
+        }
+    }
 };
 
 const logout = (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
 
-const disallowedCharactersValidator = (value) => {
-    const disallowedChars = /[!@#$%^&*(),.?":{}|<>]/;
-    return !disallowedChars.test(value);
-}
-
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         minLength: [5, 'Username should be at least 5 characters.'],
-        maxLength: [12, 'Username should not be longer than 12 characters.'],
-        validate: {
-            validator: disallowedCharactersValidator,
-            message: 'Username contains characters that are not allowed.'
-        }
+        maxLength: [15, 'Username should not be longer than 15 characters.'],
     },
     password: {
         type: String,
         required: true,
+        minLength: [5, 'Username should be at least 5 characters.'],
+        maxLength: [15, 'Username should not be longer than 15 characters.'],
     },
     attending: [{
         type: mongoose.SchemaTypes.ObjectId,
